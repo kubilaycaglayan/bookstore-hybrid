63be9e8a8a376049a8f5d8fe814dbdb435dea08a
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-10 13:57:38 +0300

    Create folder structure

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -5,27 +5,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-
-const Hello = ({ name }) => (
-  <div>
-    Hello
-    {name}
-    !
-  </div>
-);
-
-Hello.defaultProps = {
-  name: 'David',
-};
-
-Hello.propTypes = {
-  name: PropTypes.string,
-};
+import App from './components/App';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
+    <App />,
+    document.getElementById('root'),
   );
 });
