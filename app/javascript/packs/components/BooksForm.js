4ccdc2c14c10880e0e5d5a257aef26c3716524ea
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-11 11:47:30 +0300

    Add BookForm component

--- app/javascript/packs/components/BooksForm.js
+++ app/javascript/packs/components/BooksForm.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const BooksForm = () => {
+  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
+  return (
+    <>
+      <input />
+      <select>
+        {categories.map((category, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <option key={index} value={category} id="category">
+            {category}
+          </option>
+        ))}
+      </select>
+      <button type="button">Add Book</button>
+    </>
+  );
 };
 
 export default BooksForm;
