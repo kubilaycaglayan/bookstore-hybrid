51b957d9cf619357ae1d449181f8fc0a76511eca
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-11 11:42:25 +0300

    Add Book component

--- app/javascript/packs/components/App.js
+++ app/javascript/packs/components/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { createStore } from 'redux';
+import Provider from 'react-redux';
 import BooksList from './BooksList';
 import BooksForm from './BooksForm';
+import reducer from '../reducers/index';
+
+const initialState = {
+  books: [
+    {
+      id: 111,
+      title: 'Great',
+      category: 'Action',
+    },
+  ],
+};
+
+const store = createStore(reducer, initialState);
 
 const App = () => {
   return (
-    <div>
+    <Provider store={store}>
       <BooksList />
       <BooksForm />
-    </div>
+    </Provider>
   );
 };
 
--- app/javascript/packs/components/BooksList.js
+++ app/javascript/packs/components/BooksList.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Book from './Book';
 
-const BooksList = () => {
+const mapStateToProps = state => ({
+  books: state.books,
+});
 
+const BooksList = props => {
+  const { books } = props;
+
+  return (
+    <table>
+      <tr>
+        <th>BOOK ID</th>
+        <th>TITLE</th>
+        <th>CATEGORY</th>
+      </tr>
+      {books.map(book => {
+        return <Book book={book} key={book.id} />;
+      })}
+    </table>
+  );
 };
+
+BooksList.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  books: PropTypes.array.isRequired,
+};
+
+connect(
+  mapStateToProps,
+  null,
+  // mapDispatchToBooksList,
+)(
+  BooksList,
+);
 
 export default BooksList;
